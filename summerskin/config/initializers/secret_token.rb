8059284d834e368c0d66ad7d95259ba2f3db3792
brett-anderson <brettanderson16@gmail.com>
2013-11-11 10:54:39 -0600

    Added Rails

---
+++ summerskin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Summerskin::Application.config.secret_token = '5261b72653eea71f60e6e614c9aa80aa9ca6d91a1519b2cec80bcc3b400716d2d91bd2cf825a67c4b79efbe14aab80f2cf9cdf9f5850ea224248cf5d7209fc41'
