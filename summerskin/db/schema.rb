91e4453faab532a1d9afa9486a368a9ceb25843d
brett-anderson <brettanderson16@gmail.com>
2013-11-20 20:25:10 -0600

    added product to admin

--- summerskin/db/schema.rb
+++ summerskin/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131114235516) do
+ActiveRecord::Schema.define(:version => 20131121022314) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -74,6 +74,15 @@ ActiveRecord::Schema.define(:version => 20131114235516) do
     t.decimal  "gst_rate"
     t.decimal  "hst_rate"
     t.integer  "customer_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price"
+    t.integer  "quantity"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
